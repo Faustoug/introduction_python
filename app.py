b533989e5a114fc78ec6c9c18666b2a32274171f
FAUSTO <faustoug@gmail.com>
2022-02-18 21:10:49 -0500

    iniciando repositorio

---
+++ app.py
@@ -0,0 +1,22 @@
+#hola mundo
+
+'''
+    dshd
+    dsdssd
+    sdssdsd
+    sdsdsdsd
+'''
+nombre = 'hola' #String
+cantidadMaterias = 3 #Integer
+numeroDecimal = 1.7 #Float
+
+diasSemana = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes']   #array - lista
+
+listaDinamica = [0, 'hola', 12.5, [0, 1]]
+
+print(diasSemana[2])
+
+print(listaDinamica[3][1])  #imprime el uno del arreglo 
+
+
+
